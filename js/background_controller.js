5d032c9505f68d8e2d7c04e4316478647b05dcee
Mohamed Mansour <hello@mohamedmansour.com>
2011-06-30 17:54:14 -0400

    Version number when installing and updating.
    To keep track of extension version, useful when doing upgrades
    from legacy to existing.

--- js/background_controller.js
+++ js/background_controller.js
@@ -4,6 +4,44 @@
  */
 BackgroundController = function()
 {
+  this.onExtensionLoaded();
+};
+
+/**
+ * Triggered when the extension just loaded. Should be the first thing
+ * that happens when chrome loads the extension.
+ */
+BackgroundController.prototype.onExtensionLoaded = function()
+{
+  var currVersion = chrome.app.getDetails().version;
+  var prevVersion = localStorage['version'];
+  if (currVersion != prevVersion) {
+    // Check if we just installed this extension.
+    if (typeof prevVersion == 'undefined') {
+      this.onInstall();
+    } else {
+      this.onUpdate(prevVersion, currVersion);
+    }
+    localStorage['version'] = currVersion;
+  }
+};
+
+/**
+ * Triggered when the extension just installed.
+ */
+BackgroundController.prototype.onInstall = function()
+{
+};
+
+/**
+ * Triggered when the extension just uploaded to a new version. DB Migrations
+ * notifications, etc should go here.
+ *
+ * @param {string} previous The previous version.
+ * @param {string} current  The new version updating to.
+ */
+BackgroundController.prototype.onUpdate = function(previous, current)
+{
 };
 
 /**
