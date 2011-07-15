96ef7e86d74ae1b52584adcdced0713fbe88d42a
Mohamed Mansour <hello@mohamedmansour.com>
2011-07-15 01:06:21 -0400

    Design new infrastructure to support generic feeds.
    Users should be able to design their own feeds of their own networks
    currently, this is just a skeleton.

--- js/injection.js
+++ js/injection.js
@@ -1,22 +1,42 @@
-// Shared DOM.
-var originalTextNode = document.createTextNode(' \u00a0-\u00a0 ');
-var originalShareNode = document.createElement('span');
-originalShareNode.setAttribute('role', 'button');
-originalShareNode.setAttribute('class', 'd-h external-share');
-originalShareNode.innerHTML = 'Share on...';
+/**
+ * 
+ */
+Injection = function()
+{
+  this.originalTextNode = document.createTextNode(' \u00a0-\u00a0 ');
 
-var originalBubbleContainer = document.createElement('div');
-originalBubbleContainer.setAttribute('class', 'tk3N6e-Ca');
-originalBubbleContainer.setAttribute('style', 'left: 172px; margin-top: 4px');
-originalBubbleContainer.innerHTML =
-    // content
-    '<div class="tk3N6e-Ca-p-b">'+
-        '<div class="lgPbs" style="margin-right: 1em; margin-bottom: 0px;">Share on...</div>'+
-    '</div>'+
-    // cross to close
-    '<div class="tk3N6e-Ca-kmh2Gb-b tk3N6e-Ca-kmh2Gb" role="button" tabindex="0"><div class="tk3N6e-Ca-uqvIpc"></div></div>'+
-    // arrow on top
-    '<div class="tk3N6e-Ca-kc-b tk3N6e-Ca-kc tk3N6e-Ca-Hi" style="left: 20px; "><div class="tk3N6e-Ca-jQ8oHc"></div><div class="tk3N6e-Ca-ez0xG"></div></div>';
+  this.originalShareNode = document.createElement('span');
+  this.originalShareNode.setAttribute('role', 'button');
+  this.originalShareNode.setAttribute('class', 'd-h external-share');
+  this.originalShareNode.innerHTML = 'Share on...';
+
+  this.originalBubbleContainer = document.createElement('div');
+  this.originalShareNode.setAttribute('class', 'tk3N6e-Ca');
+  this.originalShareNode.setAttribute('style', 'left: 172px; margin-top: 4px');
+  this.originalShareNode.innerHTML =
+      // content
+      '<div class="tk3N6e-Ca-p-b">'+
+          '<div class="lgPbs" style="margin-right: 1em; margin-bottom: 0px;">Share on...</div>'+
+      '</div>'+
+      // cross to close
+      '<div class="tk3N6e-Ca-kmh2Gb-b tk3N6e-Ca-kmh2Gb" role="button" tabindex="0"><div class="tk3N6e-Ca-uqvIpc"></div></div>'+
+      // arrow on top
+      '<div class="tk3N6e-Ca-kc-b tk3N6e-Ca-kc tk3N6e-Ca-Hi" style="left: 20px; "><div class="tk3N6e-Ca-jQ8oHc"></div><div class="tk3N6e-Ca-ez0xG"></div></div>';
+};
+
+/**
+ * Initialize
+ */
+Injection.prototype.init = function()
+{
+  chrome.extension.onRequest.addListener(this.onExternalRequest.bind(this));
+
+  // Listen when the subtree is modified for new posts.
+  var googlePlusContentPane = document.querySelector('.a-b-f-i-oa');
+  if (googlePlusContentPane) {
+     googlePlusContentPane.addEventListener('DOMSubtreeModified', this.onGooglePlusContentModified.bind(this), false);
+  }
+};
 
 /**
  * Figures out where the direct link URL is for the post within the |dom|.
@@ -24,7 +44,8 @@ originalBubbleContainer.innerHTML =
  *
  * @param {Object<HTMLElement>} dom The parent DOM source for the item.
  */
-function parseURL(dom) {
+Injection.prototype.parseURL = function(dom)
+{
   var parent = dom.parentNode.parentNode.parentNode;
   var link = parent.querySelector('a[target="_blank"]');
   var text = '';
@@ -47,20 +68,21 @@ function parseURL(dom) {
     text: text,
     title: title
   };
-}
+};
 
 /**
  * Removes the bubble from the DOM. Same functionality as the share button.
  *
  * @param {Object<MouseEvent>} event The mouse event.
  */
-function destroyBubble(event) {
+Injection.prototype.destroyBubble = function(event)
+{
   var element = event.srcElement.parentNode.parentNode;
   if (element && element.className != 'tk3N6e-Ca') {
     element = element.parentNode.parentNode;
   }
   element.parentNode.removeChild(element);
-}
+};
 
 /**
  * Creates the social hyperlink image.
@@ -71,7 +93,8 @@ function destroyBubble(event) {
  * @param {boolean} limit True if you want to limit it to 100 chars.
  *                        later one, we will figure out the max length.
  */
-function createSocialLink(name, url, result, limit) {
+Injection.prototype.createSocialLink = function(name, url, result, limit)
+{
   var text = limit ? result.text.substring(0, 100) : result.text;
   url = url.replace('\${link}', result.link);
   url = url.replace('\${text}',  encodeURIComponent(text.trim()));
@@ -80,7 +103,7 @@ function createSocialLink(name, url, result, limit) {
   var a = document.createElement('a');
   a.setAttribute('href', url);
   a.setAttribute('style', 'margin: 0 .4em');
-  a.onclick = destroyBubble;
+  a.onclick = this.destroyBubble.bind(this);
 
   var img = document.createElement('img');
   img.setAttribute('src', chrome.extension.getURL('/img/' + name + '.png'));
@@ -89,7 +112,7 @@ function createSocialLink(name, url, result, limit) {
 
   a.appendChild(img);
   return a;
-}
+};
 
 /**
  * Creates the bubble overlay. Uses same CSS used in 
@@ -98,84 +121,88 @@ function createSocialLink(name, url, result, limit) {
  * @param {number} y The mouse y position.
  * @param {Object<HTMLElement>} src The parent DOM source for the item.
  */
-function createBubble(src, event) {
-  var bubbleContainer = originalBubbleContainer.cloneNode(true);
+Injection.prototype.createBubble = function(src, event)
+{
+  var bubbleContainer = this.originalBubbleContainer.cloneNode(true);
   var nodeToFill = bubbleContainer.querySelector('.lgPbs');
 
-  var result = parseURL(src);
+  var result = this.parseURL(src);
   if (result.status) {
-    nodeToFill.appendChild(createSocialLink('twitter', 'http://twitter.com/share?url=${link}&text=${text}', result, true));
-    nodeToFill.appendChild(createSocialLink('facebook', 'http://www.facebook.com/sharer.php?u=${link}&t=${text}', result));
-    nodeToFill.appendChild(createSocialLink('linkedin', 'http://www.linkedin.com/shareArticle?mini=true&url=${link}&title=${title}&summary=${text}', result));
+    nodeToFill.appendChild(this.createSocialLink('twitter', 'http://twitter.com/share?url=${link}&text=${text}', result, true));
+    nodeToFill.appendChild(this.createSocialLink('facebook', 'http://www.facebook.com/sharer.php?u=${link}&t=${text}', result));
+    nodeToFill.appendChild(this.createSocialLink('linkedin', 'http://www.linkedin.com/shareArticle?mini=true&url=${link}&title=${title}&summary=${text}', result));
   } else {
     nodeToFill.appendChild(document.createTextNode('Cannot find URL, please file bug to developer. hello@mohamedmansour.com'));
   }
 
   var closeCross = bubbleContainer.querySelector('.tk3N6e-Ca-kmh2Gb');
-  closeCross.onclick = destroyBubble;
+  closeCross.onclick = this.destroyBubble.bind(this);
   
   src.parentNode.appendChild(bubbleContainer);
-}
+};
 
 /**
  * On Click event for when sending the link.
  *
  * @param {Object<MouseEvent>} event The mouse event.
  */
-function onSendClick(event) {
+Injection.prototype.onSendClick = function(event)
+{
   var element = event.srcElement.parentNode.querySelector('.tk3N6e-Ca');
   if (!element) {
-    createBubble(event.srcElement, event);
+    this.createBubble(event.srcElement, event);
   }
   else {
     element.style.display = 'block';
   }
-}
+};
+
 /**
  * Render all the items in the current page.
  */
-function renderAll() {
+Injection.prototype.renderAll = function()
+{
   var actionBars = document.querySelectorAll('.a-f-i-bg');
   for (var i = 0; i < actionBars.length; i++) {
-    renderItem(actionBars[i]);
+    this.renderItem(actionBars[i]);
   }
-}
+};
 
 /**
  * Render the "Share on ..." Link on each post.
  *
  * @param {Object<ModifiedDOM>} event modified event.
  */
-function renderItem(itemDOM) {
+Injection.prototype.renderItem = function(itemDOM)
+{
   if (itemDOM && !itemDOM.classList.contains('gpi-crx')) {
-    var shareNode = originalShareNode.cloneNode(true);
-    shareNode.onclick = onSendClick;
-    itemDOM.appendChild(originalTextNode.cloneNode(true));
+    var shareNode = this.originalShareNode.cloneNode(true);
+    shareNode.onclick = this.onSendClick.bind(this);
+    itemDOM.appendChild(this.originalTextNode.cloneNode(true));
     itemDOM.appendChild(shareNode);
     itemDOM.classList.add('gpi-crx');
   }
-}
+};
 
 /**
  * Render the "Share on ..." Link on each post.
  */
-function onGooglePlusContentModified(e) {
+Injection.prototype.onGooglePlusContentModified = function(e)
+{
   if (e.target.id.indexOf('update') == 0) {
     var actionBar = e.target.querySelector('.a-f-i-bg');
-    renderItem(actionBar);
+    this.renderItem(actionBar);
   }
 }
 
 // API to handle when clicking on different HTML5 push API. This somehow doesn't
 // play well with DOMSubtreeModified
-chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+Injection.prototype.onExternalRequest = function(request, sender, sendResponse)
+{
   if (request.method == 'render') {
-    renderAll();
+    this.renderAll();
   }
-});
+};
 
-// Listen when the subtree is modified for new posts.
-var googlePlusContentPane = document.querySelector('.a-b-f-i-oa');
-if (googlePlusContentPane) {
-   googlePlusContentPane.addEventListener('DOMSubtreeModified', onGooglePlusContentModified, false);
-}
+var injection = new Injection();
+injection.init();
