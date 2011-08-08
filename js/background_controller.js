f295d31d3ec024e2c23e5cadf2ec7ccdbe3781b0
Mohamed Mansour <hello@mohamedmansour.com>
2011-08-08 17:57:47 -0400

    Fix bug when changing your first share when installed.
    The Shares enum wasn't being injected automatically when you first
    installed the application. It would have still worked if you
    hard refreshed it, but some users having issues that it wasn't
    working.

--- js/background_controller.js
+++ js/background_controller.js
@@ -31,13 +31,16 @@ BackgroundController.prototype.onExtensionLoaded = function() {
  */
 BackgroundController.prototype.onInstall = function() {
   this.doWorkTabs(function(tab) {
-    chrome.tabs.executeScript(tab.id, { file: 'js/injection.js',
+    chrome.tabs.executeScript(tab.id, { file: 'js/shares.js',
                               allFrames: true }, function() {
-      // This is needed because all the DOM is already inserted, no events
-      // would have been fired. This will force the events to fire after
-      // initial injection.
-      chrome.tabs.sendRequest(tab.id, {
-          method: 'InitialInjection'
+      chrome.tabs.executeScript(tab.id, { file: 'js/injection.js',
+                                allFrames: true }, function() {
+        // This is needed because all the DOM is already inserted, no events
+        // would have been fired. This will force the events to fire after
+        // initial injection.
+        chrome.tabs.sendRequest(tab.id, {
+            method: 'InitialInjection'
+        });
       });
     });
   });
